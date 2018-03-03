b3e7f865b7a48a24920c460ae03fedd4e172c321
Annia Benavidez <annia_benavidez@yahoo.com>
2018-03-02 21:28:40 -0600

    Lectura de datos
    Es la lectura de datos que no corre con toda la direccion debido a un error de escritura en las diagonales corregir

---
+++ l.R
@@ -0,0 +1,16 @@
+#Lectura y escritura de datos
+
+#dput y dget
+
+#1.-creo el objeto
+y <- data.frame(a=1,b="a")
+y
+#2. Estudio la manera en que se guardara
+dput(y)
+#3Lo guardo en mi disco duro
+dput(y, file="\GitHub\Programacion_Actuarial_III/y.R")
+#4. Genero un nuevo objeto a partir de la informacion guardada
+
+
+#Tarea almacenar en tu disco duro la base de datos
+#mtcars en un archivo "carros.R" y posteriormente guardarlo en una dataframe
